5423ce5112d2dade538b36b5925e965d8de32cdb
David Workman <workmad3@gmail.com>
2011-10-03 13:34:43 +0100

    Started git repository for dspace rest tests

---
+++ dspace-rest/src/test/java/uk/ac/jorum/integration/RestApiTest.java
@@ -0,0 +1,12 @@
+package uk.ac.jorum.integration;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class RestApiTest {
+
+  @Test
+  public void something() {
+    assertEquals(50, 50);
+  }
+}
