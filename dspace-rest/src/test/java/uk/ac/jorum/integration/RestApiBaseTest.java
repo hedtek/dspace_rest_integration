15a95d8a80e0f906cd1f1378a333d759f8a42987
David Workman <workmad3@gmail.com>
2011-10-03 15:45:31 +0100

    Attempting to use hsqldb for in-memory dspace db while testing

--- dspace-rest/src/test/java/uk/ac/jorum/integration/RestApiBaseTest.java
+++ dspace-rest/src/test/java/uk/ac/jorum/integration/RestApiBaseTest.java
@@ -2,8 +2,13 @@ package uk.ac.jorum.integration;
 
 import org.junit.Before;
 import org.junit.After;
+import org.junit.BeforeClass;
 
 public abstract class RestApiBaseTest {
+  @BeforeClass
+    public static void SetupDatabase() {
+    }
+
   @Before
     public void ApiSetup() {
     }
